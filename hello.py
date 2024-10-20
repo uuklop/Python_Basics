1b87bdcb258334aba985688732dc8bfb1d9e5404
uuklop <jamespolkuudery@gmail.com>
2024-10-20 23:26:21 +0800

    dummy python file

---
+++ hello.py
@@ -0,0 +1,10 @@
+def printName(name):
+	if name == "James":
+		return 1
+	else:
+		return "Polkuu"
+		
+
+nameInput = "James"
+result = printName(nameInput)
+print(f"The name of the user is {result}."}
